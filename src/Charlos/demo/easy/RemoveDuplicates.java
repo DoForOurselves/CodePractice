09a953892e4c56632f3133ff92002b63081110c6
Charlos <1173128047@qq.com>
2019-11-08 21:11:51 +0800

    完成26增加27
    26、删除排序数组中的重复项 RemoveDuplicates函数;
    27、移除元素 RemoveElement函数

--- src/Charlos/demo/easy/RemoveDuplicates.java
+++ src/Charlos/demo/easy/RemoveDuplicates.java
@@ -7,23 +7,37 @@ package Charlos.demo.easy;
  * @Date 2019/10/29 15:42
  * @Version 1.0
  **/
+/*
+    #26 删除排序数组中的重复项
+
+    给定一个排序数组，你需要在原地删除重复出现的元素，使得每个元素只出现一次，返回移除后数组的新长度。
+    不要使用额外的数组空间，你必须在原地修改输入数组并在使用 O(1) 额外空间的条件下完成。
+
+ */
 public class RemoveDuplicates {
     public static int removeDuplicates(int[] nums){
-        return back(nums);
-    }
-    public static int back(int[] nums) {
-        int deleted=0;
-        for(int i=0;i<nums.length;i++){
-            if(i+1<nums.length&&nums[i]==nums[i+1]){
-                int j=i;
-                while(j+1<nums.length){
-                    nums[j]=nums[j+1];
-                    j++;
-                }
-                deleted++;
-                i--;
+        //存放临时相同元素
+        int temple;
+        //结果
+        int result=1;
+        //判断数组长度为空
+        if(nums.length==0){
+            return 0;
+        }else{
+            temple=nums[0];
+        }
+        //数组只有1个元素
+        if(nums.length==1){
+            return 1;
+        }
+        //数组长度大于1时
+        for(int i=1;i<nums.length;i++){
+            if(nums[i]!=temple){
+                nums[result]=nums[i];
+                temple=nums[i];
+                result++;
             }
         }
-        return nums.length-deleted/2;
+        return result;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,12 @@
 import Charlos.demo.easy.RemoveDuplicates;
+import Charlos.demo.easy.RomanToInt;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         //System.out.println(ValidParentheses.isValid("]"));
-        //System.out.println(RomanToInt.romanToInt("CMXCIX"));
-        int []nums={0,0,1,1,1,2,2,3,3,4};
-        System.out.println(RemoveDuplicates.removeDuplicates(nums));
+        System.out.println(RomanToInt.romanToInt("CMXCIX"));
+//        int []nums={0,0,1,1,1,2,2,3,3,4};
+//        System.out.println(RemoveDuplicates.removeDuplicates(nums));
     }
 }
